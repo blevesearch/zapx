03660780500f0d6e647696c3474ef7ddb0c32396
Thejas-bhat <thejas.orkombu@couchbase.com>
2022-10-19 11:15:08 +0530

    added comments about the backward compatibility implementations

--- posting.go
+++ posting.go
@@ -111,6 +111,10 @@ type PostingsList struct {
 // represents an immutable, empty postings list
 var emptyPostingsList = &PostingsList{}
 
+// Following methods are dummy implementations for the interface
+// DiskStatsReporter (for backward compatibility).
+// The working implementations are supported only in zap v15.x
+// and not in the earlier versions of zap.
 func (i *PostingsList) ResetBytesRead(uint64) {}
 
 func (i *PostingsList) BytesRead() uint64 {
@@ -324,6 +328,10 @@ type PostingsIterator struct {
 
 var emptyPostingsIterator = &PostingsIterator{}
 
+// Following methods are dummy implementations for the interface
+// DiskStatsReporter (for backward compatibility).
+// The working implementations are supported only in zap v15.x
+// and not in the earlier versions of zap.
 func (i *PostingsIterator) ResetBytesRead(uint64) {}
 
 func (i *PostingsIterator) BytesRead() uint64 {

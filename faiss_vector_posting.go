blevesearch/zapx

commit 90bb10c78f82bf703c7767fa2f9b19109c5b09d3

Separate functions to read and search vector indexes. (#184)
* separate functions to read and search vector indexes.

* minor update

* addressed reviews

* common func for doc values

faiss_vector_posting.go
@@ -266,7 +266,60 @@ func (vpl *VecPostingsIterator) BytesWritten() uint64 {
 	return 0
 }
 
-func (sb *SegmentBase) SimilarVectors(field string, qVector []float32, k int64, except *roaring.Bitmap) (segment.VecPostingsList, error) {
+func (sb *SegmentBase) GetVectorIndex(field string) (segment.VectorIndex, error) {
+	var vecIndex *faiss.IndexImpl
+
+	fieldIDPlus1 := sb.fieldsMap[field]
+	if fieldIDPlus1 > 0 {
+		vectorSection := sb.fieldsSectionsMap[fieldIDPlus1-1][SectionFaissVectorIndex]
+		// check if the field has a vector section in the segment.
+		if vectorSection > 0 {
+			pos := sb.navigatingVectorDocValues(vectorSection)
+
+			// todo: not a good idea to cache the vector index perhaps, since it could be quite huge.
+			indexSize, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+			pos += n
+			indexBytes := sb.mem[pos : pos+int(indexSize)]
+			pos += int(indexSize)
+
+			var err error
+			vecIndex, err = faiss.ReadIndexFromBuffer(indexBytes, faiss.IOFlagReadOnly)
+			if err != nil {
+				return vecIndex, err
+			}
+		}
+	}
+
+	return vecIndex, nil
+}
+
+// Returns position after skipping through bytes for doc values.
+func (sb *SegmentBase) navigatingVectorDocValues(vectorSection uint64) int {
+	pos := int(vectorSection)
+
+	// loading doc values - adhering to the sections format. never
+	// valid values for vector section
+	_, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+	pos += n
+
+	_, n = binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+	pos += n
+
+	return pos
+}
+
+func (sb *SegmentBase) SearchSimilarVectors(vecIndex segment.VectorIndex, field string,
+	qVector []float32, k int64,
+	except *roaring.Bitmap) (segment.VecPostingsList, error) {
+
+	if vecIndex == nil {
+		var err error
+		vecIndex, err = sb.GetVectorIndex(field)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	// 1. returned postings list (of type PostingsList) has two types of information - docNum and its score.
 	// 2. both the values can be represented using roaring bitmaps.
 	// 3. the Iterator (of type PostingsIterator) returned would operate in terms of VecPostings.
@@ -284,20 +337,11 @@ func (sb *SegmentBase) SimilarVectors(field string, qVector []float32, k int64, 
 		vectorSection := sb.fieldsSectionsMap[fieldIDPlus1-1][SectionFaissVectorIndex]
 		// check if the field has a vector section in the segment.
 		if vectorSection > 0 {
-			pos := int(vectorSection)
-
-			// loading doc values - adhering to the sections format. never
-			// valid values for vector section
-			_, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
-			pos += n
-
-			_, n = binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
-			pos += n
+			pos := sb.navigatingVectorDocValues(vectorSection)
 
 			// todo: not a good idea to cache the vector index perhaps, since it could be quite huge.
 			indexSize, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
 			pos += n
-			indexBytes := sb.mem[pos : pos+int(indexSize)]
 			pos += int(indexSize)
 
 			// read the number vectors indexed for this field and load the vector to docID mapping.
@@ -336,12 +380,6 @@ func (sb *SegmentBase) SimilarVectors(field string, qVector []float32, k int64, 
 
 				vecDocIDMap[vecID] = []uint32{uint32(docID)}
 			}
-
-			vecIndex, err := faiss.ReadIndexFromBuffer(indexBytes, faiss.IOFlagReadOnly)
-			if err != nil {
-				return nil, err
-			}
-			defer vecIndex.Close()
 
 			scores, ids, err := vecIndex.Search(qVector, k)
 			if err != nil {

faiss_vector_test.go
@@ -391,7 +391,13 @@ func TestVectorSegment(t *testing.T) {
 	}
 
 	if vecSeg, ok := segOnDisk.(segment.VectorSegment); ok {
-		pl, err := vecSeg.SimilarVectors("stubVec", []float32{0.0, 0.0, 0.0}, 3, nil)
+		vecIndex, err := vecSeg.GetVectorIndex("stubVec")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		pl, err := vecSeg.SearchSimilarVectors(vecIndex, "stubVec",
+			[]float32{0.0, 0.0, 0.0}, 3, nil)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -441,10 +447,17 @@ func TestPersistedVectorSegment(t *testing.T) {
 	}()
 
 	if vecSeg, ok := segOnDisk.(segment.VectorSegment); ok {
-		pl, err := vecSeg.SimilarVectors("stubVec", []float32{0.0, 0.0, 0.0}, 3, nil)
+		vecIndex, err := vecSeg.GetVectorIndex("stubVec")
 		if err != nil {
 			t.Fatal(err)
 		}
+
+		pl, err := vecSeg.SearchSimilarVectors(vecIndex, "stubVec",
+			[]float32{0.0, 0.0, 0.0}, 3, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
 		itr := pl.Iterator(nil)
 
 		for {

commit 89e8af44fd80e307df45cd5389fecbb0bc154c14

Improve search path for multi kNN and more necessarily for single kNN (#190)
* Improve search path for multi kNN and also single kNN

Single kNN would've regressed a bit because of the earlier 2-API
maneuver introduced with https://github.com/blevesearch/zapx/pull/184.

* Add some commentary around InterpretVectorIndex

faiss_vector_posting.go
@@ -266,35 +266,78 @@ func (vpl *VecPostingsIterator) BytesWritten() uint64 {
 	return 0
 }
 
-func (sb *SegmentBase) GetVectorIndex(field string) (segment.VectorIndex, error) {
+// InterpretVectorIndex returns closures that will allow the caller to -
+// (1) SearchVectorIndex - search within an attached vector index
+// (2) CloseVectorIndex - close attached vector index
+//
+// These function pointers may be nil, when InterpretVectorIndex return a non-nil err.
+// It is on the caller to ensure CloseVectorIndex is invoked (sync or async) after
+// their business with the attached vector index concludes.
+func (sb *SegmentBase) InterpretVectorIndex(field string) (
+	segment.SearchVectorIndex, segment.CloseVectorIndex, error) {
+	// Params needed for the closures
 	var vecIndex *faiss.IndexImpl
+	vecDocIDMap := make(map[int64][]uint32)
 
-	fieldIDPlus1 := sb.fieldsMap[field]
-	if fieldIDPlus1 > 0 {
-		vectorSection := sb.fieldsSectionsMap[fieldIDPlus1-1][SectionFaissVectorIndex]
-		// check if the field has a vector section in the segment.
-		if vectorSection > 0 {
-			pos := sb.navigatingVectorDocValues(vectorSection)
+	searchVectorIndex := func(field string, qVector []float32,
+		k int64, except *roaring.Bitmap) (segment.VecPostingsList, error) {
+		// 1. returned postings list (of type PostingsList) has two types of information - docNum and its score.
+		// 2. both the values can be represented using roaring bitmaps.
+		// 3. the Iterator (of type PostingsIterator) returned would operate in terms of VecPostings.
+		// 4. VecPostings would just have the docNum and the score. Every call of Next()
+		//    and Advance just returns the next VecPostings. The caller would do a vp.Number()
+		//    and the Score() to get the corresponding values
+		rv := &VecPostingsList{
+			except:   nil, // todo: handle the except bitmap within postings iterator.
+			postings: roaring64.New(),
+		}
 
-			// todo: not a good idea to cache the vector index perhaps, since it could be quite huge.
-			indexSize, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
-			pos += n
-			indexBytes := sb.mem[pos : pos+int(indexSize)]
-			pos += int(indexSize)
+		if vecIndex == nil {
+			return rv, nil
+		}
 
-			var err error
-			vecIndex, err = faiss.ReadIndexFromBuffer(indexBytes, faiss.IOFlagReadOnly)
-			if err != nil {
-				return vecIndex, err
+		scores, ids, err := vecIndex.Search(qVector, k)
+		if err != nil {
+			return nil, err
+		}
+		// for every similar vector returned by the Search() API, add the corresponding
+		// docID and the score to the newly created vecPostingsList
+		for i := 0; i < len(ids); i++ {
+			vecID := ids[i]
+			docIDs := vecDocIDMap[vecID]
+			var code uint64
+			for _, docID := range docIDs {
+				if except != nil && except.Contains(docID) {
+					// ignore the deleted doc
+					continue
+				}
+				code = getVectorCode(docID, scores[i])
+				rv.postings.Add(uint64(code))
 			}
+		}
+
+		return rv, nil
+	}
+
+	closeVectorIndex := func() {
+		if vecIndex != nil {
+			vecIndex.Close()
 		}
 	}
 
-	return vecIndex, nil
-}
+	var err error
 
-// Returns position after skipping through bytes for doc values.
-func (sb *SegmentBase) navigatingVectorDocValues(vectorSection uint64) int {
+	fieldIDPlus1 := sb.fieldsMap[field]
+	if fieldIDPlus1 <= 0 {
+		return searchVectorIndex, closeVectorIndex, nil
+	}
+
+	vectorSection := sb.fieldsSectionsMap[fieldIDPlus1-1][SectionFaissVectorIndex]
+	// check if the field has a vector section in the segment.
+	if vectorSection <= 0 {
+		return searchVectorIndex, closeVectorIndex, nil
+	}
+
 	pos := int(vectorSection)
 
 	// loading doc values - adhering to the sections format. never
@@ -305,108 +348,42 @@ func (sb *SegmentBase) navigatingVectorDocValues(vectorSection uint64) int {
 	_, n = binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
 	pos += n
 
-	return pos
-}
+	// todo: not a good idea to cache the vector index perhaps, since it could be quite huge.
+	indexSize, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+	pos += n
+	indexBytes := sb.mem[pos : pos+int(indexSize)]
+	pos += int(indexSize)
 
-func (sb *SegmentBase) SearchSimilarVectors(vecIndex segment.VectorIndex, field string,
-	qVector []float32, k int64,
-	except *roaring.Bitmap) (segment.VecPostingsList, error) {
-
-	if vecIndex == nil {
-		var err error
-		vecIndex, err = sb.GetVectorIndex(field)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	// 1. returned postings list (of type PostingsList) has two types of information - docNum and its score.
-	// 2. both the values can be represented using roaring bitmaps.
-	// 3. the Iterator (of type PostingsIterator) returned would operate in terms of VecPostings.
-	// 4. VecPostings would just have the docNum and the score. Every call of Next()
-	//    and Advance just returns the next VecPostings. The caller would do a vp.Number()
-	//    and the Score() to get the corresponding values
-	rv := &VecPostingsList{
-		except:   nil, // todo: handle the except bitmap within postings iterator.
-		postings: roaring64.New(),
-	}
-
-	vecDocIDMap := make(map[int64][]uint32)
-	fieldIDPlus1 := sb.fieldsMap[field]
-	if fieldIDPlus1 > 0 {
-		vectorSection := sb.fieldsSectionsMap[fieldIDPlus1-1][SectionFaissVectorIndex]
-		// check if the field has a vector section in the segment.
-		if vectorSection > 0 {
-			pos := sb.navigatingVectorDocValues(vectorSection)
-
-			// todo: not a good idea to cache the vector index perhaps, since it could be quite huge.
-			indexSize, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+	// read the number vectors indexed for this field and load the vector to docID mapping.
+	// todo: cache the vecID to docIDs mapping for a fieldID
+	numVecs, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+	pos += n
+	for i := 0; i < int(numVecs); i++ {
+		vecID, n := binary.Varint(sb.mem[pos : pos+binary.MaxVarintLen64])
+		pos += n
+		numDocs, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+		pos += n
+		bitMap := roaring.NewBitmap()
+		// if the number docs is more than one, load the bitmap containing the
+		// docIDs, else use the optimized format where the single docID is
+		// varint encoded.
+		if numDocs > 1 {
+			bitMapLen, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
 			pos += n
-			pos += int(indexSize)
-
-			// read the number vectors indexed for this field and load the vector to docID mapping.
-			// todo: cache the vecID to docIDs mapping for a fieldID
-			numVecs, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
-			pos += n
-			for i := 0; i < int(numVecs); i++ {
-				vecID, n := binary.Varint(sb.mem[pos : pos+binary.MaxVarintLen64])
-				pos += n
-
-				numDocs, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
-				pos += n
-
-				bitMap := roaring.NewBitmap()
-
-				// if the number docs is more than one, load the bitmap containing the
-				// docIDs, else use the optimized format where the single docID is
-				// varint encoded.
-				if numDocs > 1 {
-					bitMapLen, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
-					pos += n
-
-					roaringBytes := sb.mem[pos : pos+int(bitMapLen)]
-					pos += int(bitMapLen)
-
-					_, err := bitMap.FromBuffer(roaringBytes)
-					if err != nil {
-						return nil, err
-					}
-
-					vecDocIDMap[vecID] = bitMap.ToArray()
-					continue
-				}
-				docID, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
-				pos += n
-
-				vecDocIDMap[vecID] = []uint32{uint32(docID)}
-			}
-
-			scores, ids, err := vecIndex.Search(qVector, k)
+			roaringBytes := sb.mem[pos : pos+int(bitMapLen)]
+			pos += int(bitMapLen)
+			_, err = bitMap.FromBuffer(roaringBytes)
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
-
-			// for every similar vector returned by the Search() API, add the corresponding
-			// docID and the score to the newly created vecPostingsList
-			for i := 0; i < len(ids); i++ {
-				vecID := ids[i]
-				if vecID == -1 {
-					continue // ignore the invalid entries of ids
-				}
-				docIDs := vecDocIDMap[vecID]
-				var code uint64
-
-				for _, docID := range docIDs {
-					if except != nil && except.Contains(docID) {
-						// ignore the deleted doc
-						continue
-					}
-					code = getVectorCode(docID, scores[i])
-					rv.postings.Add(uint64(code))
-				}
-			}
+			vecDocIDMap[vecID] = bitMap.ToArray()
+			continue
 		}
+		docID, n := binary.Uvarint(sb.mem[pos : pos+binary.MaxVarintLen64])
+		pos += n
+		vecDocIDMap[vecID] = []uint32{uint32(docID)}
 	}
 
-	return rv, nil
+	vecIndex, err = faiss.ReadIndexFromBuffer(indexBytes, faiss.IOFlagReadOnly)
+	return searchVectorIndex, closeVectorIndex, err
 }

go.mod
@@ -4,10 +4,10 @@ go 1.20
 
 require (
 	github.com/RoaringBitmap/roaring v1.2.3
-	github.com/blevesearch/bleve_index_api v1.1.3
+	github.com/blevesearch/bleve_index_api v1.1.4
 	github.com/blevesearch/go-faiss v1.0.3
 	github.com/blevesearch/mmap-go v1.0.4
-	github.com/blevesearch/scorch_segment_api/v2 v2.2.3
+	github.com/blevesearch/scorch_segment_api/v2 v2.2.5
 	github.com/blevesearch/vellum v1.0.10
 	github.com/golang/snappy v0.0.1
 	github.com/spf13/cobra v1.7.0

go.sum
@@ -2,14 +2,14 @@ github.com/RoaringBitmap/roaring v1.2.3 h1:yqreLINqIrX22ErkKI0vY47/ivtJr6n+kMhVO
 github.com/RoaringBitmap/roaring v1.2.3/go.mod h1:plvDsJQpxOC5bw8LRteu/MLWHsHez/3y6cubLI4/1yE=
 github.com/bits-and-blooms/bitset v1.2.0 h1:Kn4yilvwNtMACtf1eYDlG8H77R07mZSPbMjLyS07ChA=
 github.com/bits-and-blooms/bitset v1.2.0/go.mod h1:gIdJ4wp64HaoK2YrL1Q5/N7Y16edYb8uY+O0FJTyyDA=
-github.com/blevesearch/bleve_index_api v1.1.3 h1:aNyMEiWFviY/1zYm7JCr2lZRIiYX0TMtz3oymxxbApc=
-github.com/blevesearch/bleve_index_api v1.1.3/go.mod h1:PbcwjIcRmjhGbkS/lJCpfgVSMROV6TRubGGAODaK1W8=
+github.com/blevesearch/bleve_index_api v1.1.4 h1:n9Ilxlb80g9DAhchR95IcVrzohamDSri0wPnkKnva50=
+github.com/blevesearch/bleve_index_api v1.1.4/go.mod h1:PbcwjIcRmjhGbkS/lJCpfgVSMROV6TRubGGAODaK1W8=
 github.com/blevesearch/go-faiss v1.0.3 h1:NZfqZif0+OfcPVM1IDI9gjc3P3jsETR+EN54L+OlfWQ=
 github.com/blevesearch/go-faiss v1.0.3/go.mod h1:jrxHrbl42X/RnDPI+wBoZU8joxxuRwedrxqswQ3xfU8=
 github.com/blevesearch/mmap-go v1.0.4 h1:OVhDhT5B/M1HNPpYPBKIEJaD0F3Si+CrEKULGCDPWmc=
 github.com/blevesearch/mmap-go v1.0.4/go.mod h1:EWmEAOmdAS9z/pi/+Toxu99DnsbhG1TIxUoRmJw/pSs=
-github.com/blevesearch/scorch_segment_api/v2 v2.2.3 h1:apMwrcLmPrlMZ2n+SzcTJ6v0EVWjVU6PXZ+IlC+MTiw=
-github.com/blevesearch/scorch_segment_api/v2 v2.2.3/go.mod h1:ZDNK8ckaZNQ1nLz5ELvAd7BffgiNEwtkZMYF9B6rt8E=
+github.com/blevesearch/scorch_segment_api/v2 v2.2.5 h1:5SsNQmR8v1bojtGQ1zFhZravcMg5rdiX8AVu6LwlVtc=
+github.com/blevesearch/scorch_segment_api/v2 v2.2.5/go.mod h1:8N2ytOlBCdurlxDgbqsfeR1oTKRN0ZVIKdUUP1VFZNc=
 github.com/blevesearch/vellum v1.0.10 h1:HGPJDT2bTva12hrHepVT3rOyIKFFF4t7Gf6yMxyMIPI=
 github.com/blevesearch/vellum v1.0.10/go.mod h1:ul1oT0FhSMDIExNjIxHqJoGpVrBpKCdgDQNxfqgJt7k=
 github.com/cpuguy83/go-md2man/v2 v2.0.2/go.mod h1:tgQtvFlXSQOSOSIRvRPT7W67SCa46tRHOmNcaadrF8o=

faiss_vector_test.go
@@ -391,14 +391,14 @@ func TestVectorSegment(t *testing.T) {
 	}
 
 	if vecSeg, ok := segOnDisk.(segment.VectorSegment); ok {
-		vecIndex, err := vecSeg.GetVectorIndex("stubVec")
+		searchVectorIndex, closeVectorIndex, err := vecSeg.InterpretVectorIndex("stubVec")
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		pl, err := vecSeg.SearchSimilarVectors(vecIndex, "stubVec",
-			[]float32{0.0, 0.0, 0.0}, 3, nil)
+		pl, err := searchVectorIndex("stubVec", []float32{0.0, 0.0, 0.0}, 3, nil)
 		if err != nil {
+			closeVectorIndex()
 			t.Fatal(err)
 		}
 		itr := pl.Iterator(nil)
@@ -406,6 +406,7 @@ func TestVectorSegment(t *testing.T) {
 		for {
 			next, err := itr.Next()
 			if err != nil {
+				closeVectorIndex()
 				t.Fatal(err)
 			}
 			if next == nil {
@@ -413,6 +414,7 @@ func TestVectorSegment(t *testing.T) {
 			}
 			fmt.Printf("similar vec %v score %v\n", next.Number(), next.Score())
 		}
+		closeVectorIndex()
 	}
 }
 
@@ -447,14 +449,14 @@ func TestPersistedVectorSegment(t *testing.T) {
 	}()
 
 	if vecSeg, ok := segOnDisk.(segment.VectorSegment); ok {
-		vecIndex, err := vecSeg.GetVectorIndex("stubVec")
+		searchVectorIndex, closeVectorIndex, err := vecSeg.InterpretVectorIndex("stubVec")
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		pl, err := vecSeg.SearchSimilarVectors(vecIndex, "stubVec",
-			[]float32{0.0, 0.0, 0.0}, 3, nil)
+		pl, err := searchVectorIndex("stubVec",[]float32{0.0, 0.0, 0.0}, 3, nil)
 		if err != nil {
+			closeVectorIndex()
 			t.Fatal(err)
 		}
 
@@ -463,6 +465,7 @@ func TestPersistedVectorSegment(t *testing.T) {
 		for {
 			next, err := itr.Next()
 			if err != nil {
+				closeVectorIndex()
 				t.Fatal(err)
 			}
 			if next == nil {
@@ -470,5 +473,6 @@ func TestPersistedVectorSegment(t *testing.T) {
 			}
 			fmt.Printf("similar vec %v score %v\n", next.Number(), next.Score())
 		}
+		closeVectorIndex()
 	}
 }
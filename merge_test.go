8677bf73ac4d2d60f86d090735298a7aa8558d8d
Thejas-bhat <thejas.orkombu@couchbase.com>
2024-08-29 19:17:31 +0530

    unit test fixes

--- merge_test.go
+++ merge_test.go
@@ -146,7 +146,7 @@ func testMergeWithEmptySegments(t *testing.T, before bool, numEmptySegments int)
 
 		_ = os.RemoveAll("/tmp/" + fname)
 
-		emptySegment, _, err := zapPlugin.newWithChunkMode([]index.Document{}, 1024)
+		emptySegment, _, err := zapPlugin.newWithChunkMode([]index.Document{}, 1024, nil)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -644,7 +644,7 @@ func buildTestSegmentMultiHelper(docIds []string) (*SegmentBase, uint64, error) 
 		doc2,
 	}
 
-	seg, size, err := zapPlugin.newWithChunkMode(results, 1024)
+	seg, size, err := zapPlugin.newWithChunkMode(results, 1024, nil)
 	return seg.(*SegmentBase), size, err
 }
 

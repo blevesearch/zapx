8c27989cc915b66a4f8c3e7281df4517a701f7e2
Abhi Dangeti <abhinav@couchbase.com>
2025-02-05 11:31:01 -0700

    [v15] Update to RoaringBitmap/roaring's v2.4.4 (#301)
    Requires: https://github.com/blevesearch/scorch_segment_api/pull/49.

--- dict.go
+++ dict.go
@@ -17,7 +17,7 @@ package zap
 import (
 	"fmt"
 
-	"github.com/RoaringBitmap/roaring"
+	"github.com/RoaringBitmap/roaring/v2"
 	index "github.com/blevesearch/bleve_index_api"
 	segment "github.com/blevesearch/scorch_segment_api/v2"
 	"github.com/blevesearch/vellum"
